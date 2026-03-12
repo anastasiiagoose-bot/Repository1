4e1c9d1ad2de66eea6a490cd791f79afe1b0f771
“John <john@email.com>
2026-03-12 20:27:29 +0200

    New code i think

---
+++ main_1.py
@@ -0,0 +1,25 @@
+'some string 1'
+"some string 2"
+"""big string"""
+
+
+
+name='Alex'
+surname='Clinton'
+# CATS ARE THE BEST THING EXISTING EVER!!
+
+my_favourite_city = "Одеса - <>"
+link_poem = "https://osvitanova.com.ua/posts/4740-27-naipopuliarnishykh-poezii-tarasa-shevchenka"
+big_string = """"
+Встала весна, чорну землю
+Сонну розбудила,
+Уквітчала її рястом,
+Барвінком укрила;
+І на полі жайворонок,
+Соловейко в гаї
+Землю, убрану весною,
+Вранці зустрічають…
+"""""
+
+
+print(my_favourite_city)
