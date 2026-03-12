8421264ae90bb153b39c0b2209376e49be254fca
“John <john@email.com>
2026-03-12 21:19:24 +0200

    New code i think

---
+++ class work 12.py
@@ -0,0 +1,9 @@
+# cat name
+# cat fav food
+# cat food
+cat_name = "Blu"
+print("Cat name is "+cat_name)
+cat_favourite_food = "Yummy fishy food"
+print("Blu favourite food is "+cat_favourite_food)
+cat_food = "Fishy thingy, Meatish thingy, sweet potatoish."
+print("Usual Blu food to eat is "+cat_food)
